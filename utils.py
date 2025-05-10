f49a27c7db6f9f7bf3548c4750c1a5f4bb634b5d
Cameron Olson <cmsolson75@gmail.com>
2025-05-10 16:55:49 -0700

    Added batch norm as a bandade, moving to GPU

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import torch.nn as nn
+from tqdm import tqdm
+import torch
 
 def overfit_single_batch(model, loss_fn, optimizer, train_loader, epochs=20):
     model.train()
@@ -24,3 +26,35 @@ def alexnet_gaussian_init(m):
         nn.init.normal_(m.weight, mean=0.0, std=0.01)
         if m.bias is not None:
             nn.init.constant_(m.bias, 0)
+
+
+
+def train(model: nn.Module, optimizer, loss_fn, epochs, train_loader, device):
+
+    for epoch in range(epochs):
+        local_loss = 0
+        accumulation = 0
+        for x, y in tqdm(train_loader):
+            x, y = x.to(device), y.to(device)
+            logits = model(x)
+            loss = loss_fn(logits, y)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            accumulation += 1
+            local_loss += loss.item()
+        print(f"Epoch {epoch + 1}, Train Loss: {local_loss/accumulation:.4f}")
+    return model
+
+def evaluate(model, test_loader):
+    model.eval()
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for x, y in tqdm(test_loader):
+            logits = model(x)
+            _, pred = torch.max(logits, 1)
+            total += y.size(0)
+            correct += (pred == y).sum().item()
+    model.train()
+    print(f"Test Accuracy: {100 * correct // total}%")
