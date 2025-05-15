b1fdc0b3d16bb6bfa819742db168b66d902bc940
Cameron Olson <cmsolson75@gmail.com>
2025-05-15 23:35:49 +0000

    cleaned up train file and organized together

--- train.py
+++ train.py
@@ -1,40 +1,32 @@
+import hydra
 import torch
-
-torch.set_float32_matmul_precision("high")
+from omegaconf import DictConfig, OmegaConf
+from lightning import seed_everything
+import lightning as L
+from lightning.pytorch.loggers import WandbLogger
 
 from alexnet.models.cifar10_alexnet import CIFAR10AlexNet
 from alexnet.data.dataset import create_cifar10_dataloader_from_config
 from alexnet.training.classifier import ClassifierTrainingWrapper
-import torch.nn as nn
-from omegaconf import OmegaConf
-from pathlib import Path
-import lightning as L
-from lightning.pytorch.loggers import WandbLogger
-from lightning import seed_everything
 
+torch.set_float32_matmul_precision("high")
 
+@hydra.main(version_base=None, config_path="alexnet/configs", config_name="config")
+def main(cfg: DictConfig):
+    seed_everything(cfg.seed)
 
+    wandb_logger = WandbLogger(
+        project="alexnet-cifar10",
+        name=cfg.run_name,
+        config=OmegaConf.to_container(cfg, resolve=True),
+    )
+    train_loader = create_cifar10_dataloader_from_config(cfg.dataset, train=True)
+    test_loader = create_cifar10_dataloader_from_config(cfg.dataset, train=False)
+    model = CIFAR10AlexNet(cfg)
+    loss_fn = torch.nn.CrossEntropyLoss()
+    model = ClassifierTrainingWrapper(model, loss_fn, cfg.optimizer)
+    trainer = L.Trainer(**cfg.trainer, logger=wandb_logger)
+    trainer.fit(model=model, train_dataloaders=train_loader, val_dataloaders=test_loader)
 
-def load_config(path: str) -> dict:
-    path = Path(path)
-    if not path.exists():
-        raise FileNotFoundError(f"Config file not found: {path}")
-    return OmegaConf.load(path)
-
-
-config = load_config("/home/cameronolson/documents/AlexNet/alexnet/configs/config.yaml")
-seed_everything(config.seed)
-
-wandb_logger = WandbLogger(
-    project="alexnet-cifar10",
-    name="exp-001",
-    config=OmegaConf.to_container(config, resolve=True),
-)
-
-train_loader = create_cifar10_dataloader_from_config(config.dataset, train=True)
-test_loader = create_cifar10_dataloader_from_config(config.dataset, train=False)
-model = CIFAR10AlexNet(config)
-loss_fn = nn.CrossEntropyLoss()
-model = ClassifierTrainingWrapper(model, loss_fn, config.optimizer)
-trainer = L.Trainer(**config.trainer, logger=wandb_logger)
-trainer.fit(model=model, train_dataloaders=train_loader, val_dataloaders=test_loader)
+if __name__ == "__main__":
+    main()
